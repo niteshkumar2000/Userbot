8f35d3146341b83127a19a3a39301dc23c60747b
Niteshkumar <nitesh156200@gmail.com>
2020-04-04 22:33:36 +0530

    Userbot: set custom pmpermit

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -16,10 +16,10 @@ from userbot import (COUNT_PM, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN,
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
-UNAPPROVED_MSG = ("`Welcome, You are a wonderful person with a wonderful view of life.`\n\n"
-                  "`Your companionship is always an opportunity But Sed This line is not for you.`"
-                  "`So just tell why you are here ?`\n\n"
-                  "`Your message will be responded when I'm free,Don't sit here whole day coz there are lots of pending messages😶‚`")
+UNAPPROVED_MSG = ("Hello! This is an automated message.\n\n"
+                  "If you have any issues with my builds,\n"
+                  "Please report here - @nitibuilds and tag me!\n"
+                  "Your message will be responded when I'm free.")
 # =================================================================
 
 
